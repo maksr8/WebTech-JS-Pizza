ef5704c53e493439fcdcb143f63845a75749e761
maksr8 <158086853+maksr8@users.noreply.github.com>
2024-06-20 23:40:11 +0300

    Add report table

---
+++ repotoscript.js
@@ -0,0 +1,61 @@
+let cart = [];
+
+// reading items from local storage
+
+for (let key in localStorage) {
+    if (!localStorage.hasOwnProperty(key)) {
+        continue;
+    }
+    console.log(`${key}: ${localStorage.getItem(key)}`);
+    cart = JSON.parse(localStorage.user);
+}
+
+const jsonData = [
+    {
+        "Назва піци": {
+            type: "level",
+        },
+        "Розмір": {
+            type: "level",
+        },
+        "Діаметр": {
+            type: "number",
+        },
+        "Вага": {
+            type: "number",
+        },
+        "Ціна": {
+            type: "number",
+        },
+        "Кількість": {
+            type: "number",
+        },
+        "Вартість": {
+            type: "number",
+        },
+    },
+];
+
+for (let item of cart) {
+    jsonData.push({
+        "Назва піци": item.pizza.title,
+        "Розмір": item.isSmall ? "Мала" : "Велика",
+        "Діаметр": item.isSmall ? item.pizza.small_size.size : item.pizza.big_size.size,
+        "Вага": item.isSmall ? item.pizza.small_size.weight : item.pizza.big_size.weight,
+        "Ціна": item.isSmall ? item.pizza.small_size.price : item.pizza.big_size.price,
+        "Кількість": item.quantity,
+        "Вартість": (item.isSmall ? item.pizza.small_size.price : item.pizza.big_size.price) * item.quantity
+    });
+}
+
+
+
+const pivot = new WebDataRocks({
+    container: "#wdr-component",
+    toolbar: true,
+    report: {
+        dataSource: {
+            data: jsonData
+        }
+    }
+});
